05c2638d6c24f08797d97e366319117bec2a7360
hyeryun <qkrgprus123@gmail.com>
2023-02-20 22:19:56 +0900

    webview추가, fab button 수정

--- app/src/main/java/com/example/comparison/main/MainActivity.kt
+++ app/src/main/java/com/example/comparison/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.comparison.main
 
+import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
@@ -7,14 +8,18 @@ import android.text.InputType
 import android.util.Log
 import android.view.Gravity
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.comparison.R
+import com.example.comparison.WebviewActivity
 import com.example.comparison.base.BaseActivity
 import com.example.comparison.comparison.ComparisonActivity
 import com.example.comparison.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : BaseActivity(), MainContract.View {
 
@@ -22,6 +27,17 @@ class MainActivity : BaseActivity(), MainContract.View {
     private lateinit var adapter: MainAdapter
 
     private lateinit var mainPresenter: MainContract.Presenter
+
+    private var isFabOpen = false
+
+    private lateinit var fb1: FloatingActionButton
+    private lateinit var fb2: FloatingActionButton
+    private lateinit var fab: FloatingActionButton
+
+    private lateinit var fabOpen: Animation
+    private lateinit var fabClose: Animation
+    private lateinit var rotateForward: Animation
+    private lateinit var rotateBackward: Animation
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,21 +93,79 @@ class MainActivity : BaseActivity(), MainContract.View {
 
     override fun setFloatingButton() {
 
-        val fab: View = findViewById(R.id.fb_main)
+        fb1 = findViewById(R.id.fb1)
+        fb2 = findViewById(R.id.fb2)
+        fab = findViewById(R.id.fb_main)
+
+        fabOpen = AnimationUtils.loadAnimation(this, R.anim.fab_open)
+        fabClose = AnimationUtils.loadAnimation(this, R.anim.fab_close)
+        rotateForward = AnimationUtils.loadAnimation(this, R.anim.rotate_forward)
+        rotateBackward = AnimationUtils.loadAnimation(this, R.anim.rotate_backward)
+
+        // 플로팅버튼 클릭
         fab.setOnClickListener {
+
+            Toast.makeText(this, "fab clicked", Toast.LENGTH_SHORT).show()
+
+            animateFab()
+
+        }
+        // 플로팅버튼 클릭 이벤트 -> fb1 : webView 띄우기
+        fb1.setOnClickListener {
+
+            Toast.makeText(this, "fab1 clicked", Toast.LENGTH_SHORT).show()
+
+            animateFab()
+
+            val intent = Intent(this, WebviewActivity::class.java)
+            startActivity(intent)
+
+        }
+        // 플로팅버튼 클릭 이벤트 -> fb2 : url 입력 Dialog 띄우기
+        fb2.setOnClickListener {
+
+            Toast.makeText(this, "fab2 clicked", Toast.LENGTH_SHORT).show()
+
+            animateFab()
+
             val et = EditText(this)
             et.gravity = Gravity.CENTER
-            et.inputType = InputType.TYPE_CLASS_NUMBER
+            et.inputType = InputType.TYPE_CLASS_TEXT
             val builder = AlertDialog.Builder(this)
                 .setTitle("상품 url을 입력해주세요.")
                 .setView(et)
                 .setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
 
                     Log.e("사용자가 입력한 url: ", et.text.toString())
-                    mainPresenter.loadData(et.text.toString())
+//                    mainPresenter.loadData(et.text.toString())
+
 
                 })
             builder.show()
+        }
+
+    }
+    private fun animateFab() {
+        if (isFabOpen) {
+
+            fab.startAnimation(rotateBackward)
+            fb1.startAnimation(fabClose)
+            fb2.startAnimation(fabClose)
+            fb1.isClickable = false
+            fb2.isClickable = false
+
+            isFabOpen = false
+
+        } else {
+
+            fab.startAnimation(rotateForward)
+            fb1.startAnimation(fabOpen)
+            fb2.startAnimation(fabOpen)
+            fb1.isClickable = true
+            fb2.isClickable = true
+
+            isFabOpen = true
+
         }
     }
 
